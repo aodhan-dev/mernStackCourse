0e45994e3d2127712fd1549290b5aed465e948d7
Aiden Gleave <aidengleave@gmail.com>
2019-07-17 12:49:19 +0100

    Feat: User login Authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const config = require('config');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
 const User = require('../../models/User');
 
 router.get('/', auth, async (req, res) => {
@@ -12,5 +17,65 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/auth
+// @desc    Authenticate User and get token
+// @access  Public
+
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // See if user exists
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
